c1e527d33a21cd2b9904c60338921a037823e19d
andno773 <andno773@student.liu.se>
2026-04-14 12:06:49 +0200

    Started lab 1

---
+++ Lab 1/SFlab1/main.m
@@ -0,0 +1,17 @@
+
+%%First 8 measurments in mic_location are x and y positions for the "good"
+%%setup
+clc; clear; close all;
+
+
+load('dataset_1_calibration.mat');  
+
+
+[bias, std_dev, variance] = task1(tphat);
+
+disp('Bias for each sensor:');
+disp(bias);
+disp('Standard deviation for each sensor:');
+disp(std_dev);
+disp('Variance for each sensor:');
+disp(variance);
